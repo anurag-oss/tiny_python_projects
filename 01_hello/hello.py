b7744804071a987d08470485cb1ec9a3951a8c4e
Anurag Mishra <12304856+anurag-oss@users.noreply.github.com>
2021-05-26 18:18:10 -0700

    Fixed pylint suggestions

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: anurag-oss
+Purpose: Say hello
+"""
 
 import argparse
 
 
 def get_args():
+    """Get the command line arguments"""
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
@@ -12,6 +16,7 @@ def get_args():
 
 
 def main():
+    """Added the havy lifting program logic"""
     args = get_args()
     print('Hello, ' + args.name + '!')
 
