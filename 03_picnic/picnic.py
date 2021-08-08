92c4b1eeea20cdf0189d1bdad062b49847ec7a88
Anurag Mishra <12304856+anurag-oss@users.noreply.github.com>
2021-08-07 21:07:51 -0700

    Completed Excercise

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Author : anumishr <anumishr@localhost>
+Date   : 2021-08-07
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Food items to bring to the picnic',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('food_items', nargs='+', type=str, help='Food Items')
+
+    parser.add_argument('-s',
+                        '--sorted',
+                        help='A boolean flag',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    food_items = args.food_items
+    should_sort = args.sorted
+
+    if should_sort:
+        food_items.sort()
+
+    formatted_food_items = format_list_of_food(food_items)
+
+    print(f'You are bringing {formatted_food_items}.')
+
+
+def format_list_of_food(food_items):
+    formatted_food_items = ', '.join(food_items)
+    last_separator = ', and' if len(food_items) >= 3 else ' and'
+    last_comma_index = formatted_food_items.rfind(",")
+    if last_comma_index == -1:
+        return formatted_food_items
+
+    formatted_food_items_last_and = formatted_food_items[:last_comma_index] + \
+        last_separator + \
+        formatted_food_items[last_comma_index + 1:]
+
+    return formatted_food_items_last_and
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
