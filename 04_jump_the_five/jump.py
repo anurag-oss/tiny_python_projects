45f200655a0f2c34949c989ab2a728afaf347330
Anurag Mishra <12304856+anurag-oss@users.noreply.github.com>
2021-08-08 04:14:53 -0700

    passed test cases

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -13,38 +13,13 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Jump the Five',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
+    parser.add_argument('text',
                         metavar='str',
                         type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        help='The text to encode')
 
     return parser.parse_args()
 
@@ -54,17 +29,26 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    text_to_encode = args.text
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    jumper_lookup = {
+        '1': '9',
+        '2': '8',
+        '3': '7',
+        '4': '6',
+        '5': '0',
+        '6': '4',
+        '7': '3',
+        '8': '2',
+        '9': '1',
+        '0': '5'
+    }
+
+    output_list = [jumper_lookup.get(c, c) for c in text_to_encode]
+
+    encoded_text = ''.join(output_list)
+
+    print(encoded_text)
 
 
 # --------------------------------------------------
